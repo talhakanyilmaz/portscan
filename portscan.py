f5d63516f4e6a72987bc507298c11b5aecebdfbb
“Talha,KANYILMAZ” <talhakanyilmaz18@gmail.com>
2017-05-31 15:57:10 +0300

    ping kontrol eklendi.

--- portscan.py
+++ portscan.py
@@ -1,11 +1,18 @@
 import sys
 from scapy.all import *
+import ping
 
 dest_ip=sys.argv[1]
 min_port=int(sys.argv[2])
 max_port=int(sys.argv[3])
 src_port=sys.argv[4]
 
+is_host_up=ping.ping(str(dest_ip),56)
+if is_host_up==False:
+    print "[!] Host down, Cikiliyor..."
+    exit(1)
+else:
+    print "[*] Host up, Tarama baslatiliyor..."
 
 def scan_port(port):
     pkt=IP(dst=str(dest_ip))/TCP(sport=int(src_port), dport=port,flags="S")
